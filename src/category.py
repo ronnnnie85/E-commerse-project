9966d73537653138c6551f73e4fe6a27debf27dd
ronnnnie85 <ronnnnie85@gmail.com>
2025-05-03 04:28:57 +0700

    сделал 2 задачи

--- src/category.py
+++ src/category.py
@@ -16,7 +16,7 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     category_count: int = 0
     product_count: int = 0
@@ -27,11 +27,27 @@ class Category:
         Args:
             name: Название категории
             description: Описание категории
-            products: Список товаров в категории
+            __products: Список товаров в категории
         """
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+
+
+    @property
+    def products(self) -> str:
+        res = []
+        for product in self.__products:
+            res.append(f"{product.name}, {product.price} руб. Остаток {product.quantity} шт.\n")
+
+        return "".join(res)
+
+
+    
