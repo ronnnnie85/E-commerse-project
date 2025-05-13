5737d7b86fdf7bc3baebd35c450693c817b91ad6
ronnnnie85 <ronnnnie85@gmail.com>
2025-05-14 04:02:09 +0700

    заготовки тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
 from src.category import Category
+from src.lawngrass import LawnGrass
 from src.product import Product
+from src.smartphone import Smartphone
 
 
 @pytest.fixture
@@ -65,3 +67,23 @@ def new_product_dct():
         "price": 160000.0,
         "quantity": 5,
     }
+
+
+@pytest.fixture
+def smartphone1():
+    return Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+
+
+@pytest.fixture
+def smartphone2():
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+
+
+@pytest.fixture
+def lawngrass1():
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+
+@pytest.fixture
+def lawngrass2():
+    return LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
