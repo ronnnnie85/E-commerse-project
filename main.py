3b58335ff2002abeac6ab2716a8cc0d6902ee7ef
ronnnnie85 <ronnnnie85@gmail.com>
2025-05-14 03:56:12 +0700

    сделал 3 задачу

--- main.py
+++ main.py
@@ -66,19 +66,19 @@ if __name__ == '__main__':
         print("Возникла ошибка TypeError при попытке сложения")
     else:
         print("Не возникла ошибка TypeError при попытке сложения")
-    #
-    # category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
-    # category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
-    #
-    # category_smartphones.add_product(smartphone3)
-    #
-    # print(category_smartphones.products)
-    #
-    # print(Category.product_count)
-    #
-    # try:
-    #     category_smartphones.add_product("Not a product")
-    # except TypeError:
-    #     print("Возникла ошибка TypeError при добавлении не продукта")
-    # else:
-    #     print("Не возникла ошибка TypeError при добавлении не продукта")
+
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+
+    category_smartphones.add_product(smartphone3)
+
+    print(category_smartphones.products)
+
+    print(Category.product_count)
+
+    try:
+        category_smartphones.add_product("Not a product")
+    except TypeError:
+        print("Возникла ошибка TypeError при добавлении не продукта")
+    else:
+        print("Не возникла ошибка TypeError при добавлении не продукта")
--- src/category.py
+++ src/category.py
@@ -44,6 +44,9 @@ class Category:
         if isinstance(product, Product):
             self.__products.append(product)
             Category.product_count += 1
+        else:
+            raise TypeError
+
 
     @property
     def products(self) -> str:
